e88b60072f70ebd86513df45cdcd89aced7b7595
jessykate <jessy@jessykate.com>
2012-12-09 15:46:32 -0800

    question numbering in survey response display

---
+++ survey/templatetags/survey_extras.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library()
+
+class CounterNode(template.Node):
+
+  def __init__(self):
+    self.count = 0
+
+  def render(self, context):
+    self.count += 1
+    return self.count
+
+@register.tag
+def counter(parser, token):
+  return CounterNode()
