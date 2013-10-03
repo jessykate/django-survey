266e769c38ae237ceb5a4f3e4341b7bfdd6b2554
Fabien Schwob <github@x-phuture.com>
2013-10-03 23:22:13 +0200

    * Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name="survey",
+    version="0.1",
+    author="Jessy Kate Schingler",
+    author_email="jessy@jessykate.com",
+    license="AGPL",
+    url="https://github.com/jessykate/django-survey",
+    packages=[
+        "survey",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)
