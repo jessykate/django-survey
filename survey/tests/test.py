93b58bc0ae556947b9b361972e4be58cc54b9917
Pierre Sassoulas <pierre.sassoulas@cea.fr>
2017-03-14 16:46:08 +0100

    Test - Added skeleton for tests
    Added a fixtures with 2 example survey and a BaseTest
    test case loading this fixture.

---
+++ survey/tests/test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from survey.tests.base_test import BaseTest
+
+
+class TestTest(BaseTest):
+
+    def test_everything(self):
+        self.fail("There is no test at all for this app.")
